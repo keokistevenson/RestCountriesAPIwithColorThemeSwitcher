dc4e54d455f2285f31d39e774752748ac72ec38a
keokistevenson <keokistevenson@gmail.com>
2026-04-10 11:21:58 -0400

    Still working on folder names. I changed the folder name of apiService in dist previously not src. So I had to redo everything.

--- dist/apiService/apiService.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function fetchData(): Promise<void>;
-//# sourceMappingURL=apiService.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Use asynchronous functions to fetch product data and display it.
+// async function productData(): Promise<void> {
+// try {
+//     const products = await fetchData();
+//     // Instantiate Products
+//     for (const item of products) {
+//         // Pass the whole object!
+//         const product = new Product(item);
+//         // this is not how i think.
+//         const discountAmount = calculateDiscount(product.price, product.discountPercentage);
+//         const discountedPrice = product.getPriceWithDiscount();
+//         const tax = calculateTax(product.price, product.category);
+//         console.log(product.displayDetails());
+//         console.log(`Discount Amount: $${discountAmount.toFixed(2)}`);
+//         console.log(`Price After Discount: $${discountedPrice.toFixed(2)}`);
+//         console.log(`Tax: $${tax.toFixed(2)}`);
+//     }
+//     } catch(error) {
+//         console.error("Application error:", error);
+//     }
+// }
+// productData();
 //# sourceMappingURL=index.js.map
